fee3bd24718276ad98a5fbaef1a2c0e4244d36d6
Silas Augusto <augusto.fernandes775@gmail.com>
2022-10-15 17:11:22 -0300

    feat: add checker
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,13 @@ import downloadQueue from "./download-queue";
 import express, { Request, Response } from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.post("/download", async (req: Request, res: Response) => {
-  const { url } = req.body;
+app.get("/download", async (req: Request, res: Response) => {
+  const { url } = req.query;
   try {
     await downloadQueue.getQueue().add({ url });
     return res.status(200).send();
@@ -19,6 +19,10 @@ app.post("/download", async (req: Request, res: Response) => {
     console.log(error);
     return res.status(400).json({ error: "error" });
   }
+});
+
+app.get("/", async (req: Request, res: Response) => {
+  return res.json({ ok: true });
 });
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
